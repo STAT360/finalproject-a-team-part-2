ea937c68443b8502a2ee0b62d4320482c23ab85b
kristenrutschke <kristenrutschke@yahoo.com>
2019-05-17 17:25:44 +0000

    Created drop down menu to choose poll id; changed bars to blue

--- App-1/app.r
+++ App-1/app.r
@@ -1,10 +1,27 @@
 library(shiny)
 library(dplyr)
 library(plotly)
-if (interactive()) {
+candidates <- read.csv(file="president_primary_polls.csv", header=TRUE, sep=",")
+vec<-candidates%>%
+  select(poll_id)%>%
+  distinct()
+
+#if (interactive()) {
   ui <- fluidPage(
-    numericInput("PollId", "Poll Id:", 57975, min = 56000, max = 59000),
-    verbatimTextOutput("value"),
+  
+    #numericInput("PollId", "Poll Id:", 57975, min = 56000, max = 59000),
+    #verbatimTextOutput("value"),
+    selectInput("var",
+                label="Choose a polling ID: ",
+                choices = vec
+                
+                
+                ),
+    #selectInput("id",
+                #label="Choose a polling ID: ",
+                #choices = 
+      
+    #),
     
     titlePanel("Democratic Primary Candidate Frontrunner Data"),
       mainPanel(plotOutput("distPlot"))
@@ -12,23 +29,23 @@ if (interactive()) {
     
   )
   server <- function(input, output) {
-    candidates <- read.csv(file="president_primary_polls.csv", header=TRUE, sep=",")
+    #candidates <- read.csv(file="president_primary_polls.csv", header=TRUE, sep=",")
     #only include relevant candidates
-    candidates <- filter(candidates, pct > 10)
+    candidates <- filter(candidates, pct > 5)
     candidates <- filter(candidates, party == "DEM")
-    output$value <- renderPrint({ 
-        candidates[candidates$poll_id == {input$PollId}, "pollster"][1]
-      })
+    #output$value <- renderPrint({ 
+        #candidates[candidates$poll_id == {input$PollId}, "pollster"][1]
+      #})
     output$distPlot <- renderPlot({
-      candidates <- filter(candidates, poll_id == { input$PollId })
+      candidates <- filter(candidates, poll_id == { input$var })
       #change y value to percentage instead of count
       #ggplot(data = candidates)+geom_bar(mapping = aes(x = answer), fill = "blue")
       ggplot(data=candidates, aes(x=answer, y=pct)) +
-        geom_bar(stat="identity")
+        geom_bar(stat="identity",fill="blue")
     })
     
   }
-  if(interactive()){
+  #if(interactive()){
     shinyApp(ui = ui, server = server)
-  }
-}
+  #}
+#}
