cb3d46ccd1ca1869844a36c64ca28ab027be9560
kristenrutschke <kristenrutschke@yahoo.com>
2019-05-20 21:27:33 +0000

    styled code with "styler"

--- App-1/app.r
+++ App-1/app.r
@@ -4,15 +4,7 @@ library(plotly)
 library(RColorBrewer)
 library(base)
 candidates <- read.csv(file="president_primary_polls.csv", header=TRUE, sep=",")
-#vec<-candidates%>%
-  #filter(party=="DEM")%>%
-  #select(question_id)%>%
-  #distinct()
-#vec2<-candidates%>%
-  #filter(party=="DEM")%>%
-  #select(pollster)%>%
-  #distinct()
-winners<-candidates%>%
+winners <- candidates%>%
   filter(party=="DEM")%>%
   group_by(question_id)%>%
   filter(pct==max(pct))%>%
@@ -20,43 +12,36 @@ winners<-candidates%>%
   select(answer)%>%
   distinct()
 
-#if (interactive()) {
   ui <- fluidPage(
-  
-    #numericInput("PollId", "Poll Id:", 57975, min = 56000, max = 59000),
-    #verbatimTextOutput("value"),
-    checkboxGroupInput("method",
-                       "Choose the method of poll: ",
-                       choiceNames = c("Online", "Live Phone", "IVR/Online", "Online/IVR","Unspecified"),
-                       choiceValues = c("Online","Live Phone","IVR/Online","Online/IVR","")
-                       #choices = c("Online","Live Phone","IVR/Online","Online/IVR","")
-                       ),
-    selectInput("pster",
-                label="Choose a pollster: ",
-                choices = NULL
-                  ),
-    selectInput("var",
-                label="Choose a question ID: ",
-                choices = NULL
-                
-                
-                ),
-    numericInput("percent",label="Choose the lowest percent to display: ",5, min = 0, max = 100),
-    
-    
     titlePanel("Democratic Primary Candidate Data"),
+    sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput("method",
+                           "Choose the method of poll: ",
+                           choiceNames = c("Online", "Live Phone", "IVR/Online", "Online/IVR","Unspecified"),
+                           choiceValues = c("Online","Live Phone","IVR/Online","Online/IVR","")
+                           #choices = c("Online","Live Phone","IVR/Online","Online/IVR","")
+        ),
+        selectInput("pster",
+                    label="Choose a pollster: ",
+                    choices = NULL
+        ),
+        selectInput("var",
+                    label="Choose a question ID: ",
+                    choices = NULL
+        ),
+        numericInput("percent",label="Choose the lowest percent to display: ",5, min = 0, max = 100)
+        
+      ),
       mainPanel(plotOutput("distPlot"),
                 textOutput("selected_var2"),
                 textOutput("selected_var3"),
                 uiOutput(outputId = "my_ui")
-                #add some user interaction
-  )
- 
+      )
+      
+    )
   )
   server <- function(input, output,session) {
-    #candidates <- read.csv(file="president_primary_polls.csv", header=TRUE, sep=",")
-    #only include relevant candidates
-    #candidates <- filter(candidates, pct > 5)
     observeEvent(input$method,{
       updateSelectInput(session,'pster',
                         choices = candidates[candidates$methodology %in% {input$method},"pollster"] 
@@ -68,9 +53,6 @@ winners<-candidates%>%
                         )
     })
     candidates <- filter(candidates, party == "DEM")
-    #output$value <- renderPrint({ 
-        #candidates[candidates$poll_id == {input$PollId}, "pollster"][1]
-      #})
     output$selected_var2 <- renderText({
       
       paste("Time of poll: ", candidates[candidates$question_id == {input$var},"start_date"][1] , "to",candidates[candidates$question_id == {input$var},"end_date"][1]  )
@@ -84,11 +66,10 @@ winners<-candidates%>%
     output$distPlot <- renderPlot({
       candidates <- filter(candidates, question_id == { input$var })
       candidates <- filter(candidates, pct >= {input$percent})
-      #change y value to percentage instead of count
-      #ggplot(data = candidates)+geom_bar(mapping = aes(x = answer), fill = "blue")
       ggplot(data=candidates, aes(x=answer, y=pct, fill = pct)) +
         geom_bar(stat="identity") +
         theme(axis.text.x=element_text(angle=60, hjust=1)) +
+        ggtitle("Democratic Primary Poll Results") +
         xlab("Candidate Name") + 
         ylab("Percent") + 
         scale_fill_gradient(low= "lightblue", high = "darkblue")
@@ -114,7 +95,7 @@ winners<-candidates%>%
         img(src = 'Kamala_Harris.jpg', height = '300px')
       }
       else if(isTRUE(win$answer == "Obama")){
-        img(src = 'Beto_ORourke.jpg', height = '300px')
+        img(src = 'Michelle_Obama.jpg', height = '300px')
       }else{
         img(src = 'Unknown_portrait.jpg', height = '300px')
       }
@@ -122,7 +103,5 @@ winners<-candidates%>%
     })
     
   }
-  #if(interactive()){
     shinyApp(ui = ui, server = server)
-  #}
 #}
